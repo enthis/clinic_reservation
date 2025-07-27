c9dfbfbbc3ed596782aa602a78968b4f9471e116
= <harits_nurrois@venteny.com>
2025-07-27 16:00:30 +0700

    feat: delete card page

--- resources/views/filament/widgets/create-appointment-button.blade.php
+++ resources/views/filament/widgets/create-appointment-button.blade.php
@@ -1,10 +1,8 @@
-<x-filament::card>
-    <div class="flex justify-end">
-        <x-filament::button
-            tag="a"
-            href="{{ route('landing') }}"
-            icon="heroicon-o-calendar">
-            Create Appointment
-        </x-filament::button>
-    </div>
-</x-filament::card>
+<div class="flex justify-end">
+    <x-filament::button
+        tag="a"
+        href="{{ route('landing') }}"
+        icon="heroicon-o-calendar">
+        Create Appointment
+    </x-filament::button>
+</div>
